c5ec639334bf73fab6de26b49cfcf7e5574912a2
Antonio <eltony162@gmail.com>
2017-01-09 15:33:33 -0600

    ECDSA Sign

--- CetificateFileEncrypt/Program.cs
+++ CetificateFileEncrypt/Program.cs
@@ -1,9 +1,4 @@
-﻿using Org.BouncyCastle.Crypto.Prng;
-using Org.BouncyCastle.Math;
-using Org.BouncyCastle.Security;
-using Org.BouncyCastle.Utilities;
-using Org.BouncyCastle.X509;
-using System;
+﻿using System;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -14,31 +9,53 @@ namespace CetificateFileEncrypt
     {
         public static void Main(string[] args)
         {
-            ReadPFXwithBouncyCastle();
+            Encrypt(@"C:\Users\Linko\Google Drive\SISA_Web\Documents\Cifrado\CERTM\20161016.CERTM\mvolariscom.pfx", "0s1a0t2pra$V", "M1SALAZARCONTRERAS/MAR W9CJJW MDWDGOY4 0945 306Y010A0025 347>1181  6306BY4 0036982232001290360G2001516701                          ");
+            Encrypt(@"C:\Users\Linko\Google Drive\SISA_Web\Documents\Cifrado\CERTM\CERTM\mvolariscom.pfx", "0s1a0t2pra$V", "M1SALAZARCONTRERAS/MAR W9CJJW MDWDGOY4 0945 306Y010A0025 347>1181  6306BY4 0036982232001290360G2001516701                          ");
 
             Console.ReadLine();
         }
 
         /// <summary>
-        /// Reads the pf xwith bouncy castle.
+        /// Encrypts the specified path.
         /// </summary>
-        private static void ReadPFXwithBouncyCastle()
+        /// <param name="path">The path.</param>
+        /// <param name="password">The password.</param>
+        /// <param name="text">The text.</param>
+        /// <returns></returns>
+        private static string Encrypt(string path, string password, string text)
         {
-            var path = @"C:\Users\Linko\Google Drive\SISA_Web\Documents\Cifrado\CERTM\CERTM\mvolariscom.pfx";
-            var password = "0s1a0t2pra$V";
+            var encoded = string.Empty;
+            var cert = new X509Certificate2();
+            var byteConverter = new ASCIIEncoding();
+            var collection = new X509Certificate2Collection();
 
-            //var collection = new X509Certificate2Collection();
+            try
+            {
+                var data = byteConverter.GetBytes(text);
+                collection.Import(path, password, X509KeyStorageFlags.PersistKeySet);
+                cert = collection[0];
 
-            //collection.Import(path, password, X509KeyStorageFlags.PersistKeySet);
+                using (ECDsa ecdsa = cert.GetECDsaPrivateKey())
+                {
+                    if (ecdsa != null)
+                    {
+                        encoded = Convert.ToBase64String(ecdsa.SignData(data, HashAlgorithmName.SHA256));
+                    }
+                    else
+                    {
+                        var publicKey = cert.PublicKey.Key as RSACryptoServiceProvider;
+                        var encryptedBytes = new byte[0];
+                        encryptedBytes = publicKey.Encrypt(data, false);
+                        encoded = Convert.ToBase64String(encryptedBytes);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
-            //var certificate = collection[0];
-
-            //var publicKey = certificate.PublicKey.Key as RSACryptoServiceProvider;
-
-            //var plaintextMessage = Encoding.UTF8.GetBytes("Hola mundo");
-
-            //var encryptedData = publicKey.Encrypt(plaintextMessage, false);
-
+            return encoded;
         }
     }
 }
