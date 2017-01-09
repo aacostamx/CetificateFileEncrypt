874f499d43a24bc882902895a8f6df38923e53eb
Antonio <eltony162@gmail.com>
2017-01-09 11:12:14 -0600

    Add project files.

---
+++ CetificateFileEncrypt/Crypto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CetificateFileEncrypt
+{
+    static public class Crypto {    
+
+    public static string Encrypt(string textToEncrypt) {
+        string certPath = @"C:\Users\Linko\Google Drive\SISA_Web\Documents\Cifrado\CERTM\20161016.CERTM\mvolariscom.pfx";
+        string certPass = "0s1a0t2pra$V";
+
+        // Create a collection object and populate it using the PFX file
+        X509Certificate2Collection collection = new X509Certificate2Collection();
+        collection.Import(certPath, certPass, X509KeyStorageFlags.PersistKeySet);
+
+        X509Certificate2 myCert = new X509Certificate2();
+        foreach (X509Certificate2 cert in collection)
+        {
+            Console.WriteLine("Subject is: '{0}'", cert.Subject);
+            Console.WriteLine("Issuer is:  '{0}'", cert.Issuer);
+            myCert = cert;
+            // Import the certificate into an X509Store object
+        }
+
+        ASCIIEncoding byteConverter = new ASCIIEncoding();
+        byte[] encodedBytes = byteConverter.GetBytes("M1SALAZARCONTRERAS/MAR W9CJJW MDWDGOY4 0945 306Y010A0025 347>1181  6306BY4 0036982232001290360G2001516701                          ");
+
+        RSACryptoServiceProvider rsa = myCert.PublicKey.Key as RSACryptoServiceProvider;
+        byte[] encryptedBytes;
+        encryptedBytes = rsa.Encrypt(encodedBytes, false);
+
+        string encodedString = Convert.ToBase64String(encryptedBytes);
+
+        return encodedString;
+    }
+
+   
+
+   } //End of class Crypto
+}
---
+++ CetificateFileEncrypt/Program.cs
@@ -0,0 +1,44 @@
+﻿using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.X509;
+using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CetificateFileEncrypt
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadPFXwithBouncyCastle();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Reads the pf xwith bouncy castle.
+        /// </summary>
+        private static void ReadPFXwithBouncyCastle()
+        {
+            var path = @"C:\Users\Linko\Google Drive\SISA_Web\Documents\Cifrado\CERTM\CERTM\mvolariscom.pfx";
+            var password = "0s1a0t2pra$V";
+
+            //var collection = new X509Certificate2Collection();
+
+            //collection.Import(path, password, X509KeyStorageFlags.PersistKeySet);
+
+            //var certificate = collection[0];
+
+            //var publicKey = certificate.PublicKey.Key as RSACryptoServiceProvider;
+
+            //var plaintextMessage = Encoding.UTF8.GetBytes("Hola mundo");
+
+            //var encryptedData = publicKey.Encrypt(plaintextMessage, false);
+
+        }
+    }
+}
